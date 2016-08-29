4ddc2e9d35a162e72d66072d896c619fcab8379f
Murat Gurel <murat@gram.gs>
2016-08-29 14:28:30 +0300

    Adds verbose option for verbose logging

--- SimpleBenchRunner/Program.cs
+++ SimpleBenchRunner/Program.cs
@@ -8,13 +8,15 @@ namespace SimpleBench.Runner
 	{
 		public static void Main(string[] args)
 		{
+			bool isVerbose = false;
 			string dllPath = null;
 			string outPath = Directory.GetCurrentDirectory();
 
 			OptionSet optionSet = new OptionSet
 			{
 				{ "dll=", "The path of DLL containing benchmarks", path => dllPath = path },
-				{ "o|out=", "The output path for reports", path => outPath = path }
+				{ "o|out=", "The output path for reports", path => outPath = path },
+				{ "v", "Verbose logging", v => isVerbose = (v != null)}
 			};
 
 			try
@@ -31,8 +33,11 @@ namespace SimpleBench.Runner
 					dllPath += ".dll";
 				}
 
-				Console.WriteLine("DLL Path: " + dllPath);
-				Console.WriteLine("Out Path: " + outPath);
+				if (isVerbose)
+				{
+					Console.WriteLine("DLL Path: " + dllPath);
+					Console.WriteLine("Out Path: " + outPath);	
+				}
 			}
 			catch (OptionException e)
 			{

751b3bbd87f72616741025a0f5c7815505cdbcbd
Murat Gurel <murat@gram.gs>
2016-08-29 14:06:34 +0300

    Adds iterations attribute

---
+++ SimpleBench/IterationsAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SimpleBench
+{
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+	public class IterationsAttribute : Attribute
+	{
+		public readonly int value;
+
+		public IterationsAttribute(int value)
+		{
+			this.value = value;
+		}
+	}
+}

a2a89cee34126285a0e4e11c30696860e1cff77f
Murat Gurel <murat@gram.gs>
2016-08-29 14:56:00 +0300

    Adds a reporting class for reporting related calculations

---
+++ SimpleBenchRunner/Reporting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleBench.Runner
+{
+	public static class Reporting
+	{
+		public static double CalculateStdDev(IEnumerable<double> values)
+		{
+			if (values.Any())
+			{
+				double avg = values.Average();
+				double sum = values.Sum(d => Math.Pow(d - avg, 2));
+				return Math.Sqrt((sum) / (values.Count() - 1));
+			}
+
+			return 0;
+		}
+	}
+}
+
